0b3cc7b281a1dc0190916a41aceca2860e79ea9a
everestq <sergey_shelepov@list.ru>
2019-09-25 01:07:57 +0300

    added section about/phone/slider/news/footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
 $(function(){
 
+    $('.slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
 
 });
